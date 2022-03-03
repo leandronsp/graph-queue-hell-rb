5b93e710288fe746f8102da5819c2f7dcd779c18
leandronsp <leandronsp@gmail.com>
2022-03-03 13:39:19 -0300

    cleanup && eye candy

--- app/mutation_type.rb
+++ app/mutation_type.rb
@@ -1,10 +1,7 @@
 require 'graphql'
 
 class MutationType < GraphQL::Schema::Object
-  field :test_field,
-    String,
-    null: false,
-    description: "An example field added by the generator"
+  field :test_field, String
 
   def test_field
     "Hello World Mutation!"
--- app/routes.rb
+++ app/routes.rb
@@ -17,44 +17,24 @@ class Routes
   def process
     route = ROUTES_TABLE["#{@verb} #{@path}"]
 
-    raise StandardError unless respond_to?(route)
+    raise 'Route not found' unless route
 
     send(route)
   end
 
   def graphql_route
-    variables = prepare_variables(@params[:variables])
-    query = @params[:query]
+    query          = @params[:query]
+    variables      = @params[:variables]
     operation_name = @params[:operationName]
-    context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
-    }
-    result = AppSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result         = AppSchema.execute(query,
+                                       variables: variables,
+                                       context: {},
+                                       operation_name: operation_name)
 
     {
       status: 200,
       headers: { 'Content-Type' => 'application/json' },
       body: result.to_json
     }
-  end
-
-  private
-
-  def prepare_variables(variables_param)
-    case variables_param
-    when String
-      if variables_param.present?
-        JSON.parse(variables_param) || {}
-      else
-        {}
-      end
-    when Hash
-      variables_param
-    when nil
-      {}
-    else
-      raise ArgumentError, "Unexpected parameter: #{variables_param}"
-    end
   end
 end
